dd0eb23d7c30749683fabe84d3296b21c4ca2dd9
your name here <echen831@gmail.com>
2020-10-07 15:11:07 -0700

    add css

--- src/component/dnd.jsx
+++ src/component/dnd.jsx
@@ -14,16 +14,50 @@ const INITIALDATA = [
 export const DND = (props) => {
 
     const [ list, setList ] = useState(INITIALDATA);
+    const [dragging, setDragging] = useState(false);
+    const [ editTitle, setEditTitle ] = useState(false);
+
+    const dragItem = useRef();
+    const dragNode = useRef();
+    const textInput = useRef();
+
+    const handleDragStart = (e, params) => {
+        dragItem.current = params;
+        dragNode.current = e.target;
+        dragNode.current.addEventListener('dragend', handleDragEnd)
+        setTimeout( () => {
+            setDragging(true)
+        },0)
+    };
+
+    const handleDragEnter = (e, params) => {
+        const currentItem = dragItem.current;
+        if (e.target !== dragNode.current) {
+            setList(oldList => {
+                let newList = JSON.parse(JSON.stringify(oldList));
+                newList[params.grpIdx].items.splice(params.itemIdx, 0, newList[currentItem.grpIdx].items.splice(currentItem.itemIdx, 1)[0])
+                dragItem.current = params;
+                return newList
+            })
+        }
+    }
+
+    const handleDragEnd = () => {
+        setDragging(false);
+        dragNode.current.removeEventListener('dragend', handleDragEnd);
+        dragItem.current = null;
+        dragNode.current = null;
+    }
 
     return (
         <div className='dnd-header'>
-            <div className='dnd-column'>
-                {list.map((col, colIdx) => (
-                    <div key={colIdx}
+            <div className='drag-n-drop'>
+                {list.map((grp, grpIdx) => (
+                    <div key={grpIdx}
                          className='dnd-item'
                          draggable
                     >
-                    {col.title}
+                    {grp.title}
                     </div>
                 ))}
             </div>
