02da5682be9845b56d3c40a3797212e1eb72bda9
your name here <echen831@gmail.com>
2020-10-07 17:53:21 -0700

    add css

--- src/component/dnd.jsx
+++ src/component/dnd.jsx
@@ -49,15 +49,71 @@ export const DND = (props) => {
         dragNode.current = null;
     }
 
+    const addItems = (col) => {
+        setList(oldList => {
+            let newList = JSON.parse(JSON.stringify(oldList))
+            newList[col].items.push({ title: '', details: '' })
+            return newList
+        })
+    }
+
+
+    const removeItem = (params) => {
+        setList(oldList => {
+            let newList = JSON.parse(JSON.stringify(oldList))
+            newList[params.grpIdx].items = newList[params.grpIdx].items.slice(0, params.itemIdx).concat(newList[params.grpIdx].items.slice(params.itemIdx + 1))
+            return newList
+
+        })
+    }
+
+    const update = (text, params) => {
+        setList(oldList => {
+            let newList = JSON.parse(JSON.stringify(oldList))
+            newList[params.grpIdx].items[params.itemIdx].title = text
+            return newList
+        })
+        textInput.current.value = ''
+        setEditTitle(false)
+    }
+
+    const getStyles = (params) => {
+        const currentItem = dragItem.current
+        if (currentItem.grpIdx === params.grpIdx && currentItem.itemIdx === params.itemIdx) {
+            return 'current dnd-item'
+        }
+        return 'dnd-item'
+    }
+
     return (
         <div className='dnd-header'>
             <div className='drag-n-drop'>
                 {list.map((grp, grpIdx) => (
                     <div key={grpIdx}
-                         className='dnd-item'
-                         draggable
+                        className='dnd-group'
+                        draggable
+                        onDragEnter={dragging && !grp.items.length ? (e) => handleDragEnter(e, { grpIdx, itemIdx: 0 }) : null}
                     >
-                    {grp.title}
+                        <div className='group-title'>{grp.title}</div>
+                        {grp.items ? grp.items.map((item, itemIdx) => (
+                            <div draggable
+                                onDragStart={(e) => { handleDragStart(e, { grpIdx, itemIdx }) }}
+                                onDragEnter={dragging ? (e) => { handleDragEnter(e, { grpIdx, itemIdx }) } : null}
+                                key={itemIdx}
+                                className={dragging ? getStyles({ grpIdx, itemIdx }) : 'dnd-item'}>
+
+                                <div>
+                                    <p>{item.title.length ? item.title : 'Title'}</p>
+                                    <button onClick={() => setEditTitle(!editTitle)}>+</button>
+                                    <div id={!editTitle ? 'hide' : ''}>
+                                        <input type="text" ref={textInput} />
+                                        <button onClick={() => update(textInput.current.value, { grpIdx, itemIdx })}>Update</button>
+                                    </div>
+                                    <button onClick={() => removeItem({ grpIdx, itemIdx })}>Remove</button>
+                                </div>
+                            </div>
+                        )) : null}
+                        <button onClick={() => addItems(grpIdx)}>Add</button>
                     </div>
                 ))}
             </div>
